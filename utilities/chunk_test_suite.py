08304965268e8ca3caf1e979e97f3dda672813b6
Amadeus Gebauer <amadeus.gebauer@tum.de>
2024-12-06 17:57:19 +0100

    Chunk test suite by test timeout*num_procs

--- utilities/chunk_test_suite.py
+++ utilities/chunk_test_suite.py
@@ -5,32 +5,26 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
+import json
 import argparse
-import re
-
-"""
-This script analyzes the log files of a test suite run and determines contiguous chunks of tests such that all chunks
-have roughly the same run time.
-"""
+import junitparser
+import os
 
 
-def find_run_time(log_file):
-    """
-    Regex needs to match lines like:
-     Test #585: elch_3D_tet4_s2i_butlervolmer_mortar_standard-p3 ........................................................   Passed    3.65 sec
-    """
-    # Find all matches of the regex. Save in a dict with the test number as the key and the run time as the value.
-    regex = re.compile(r"Test\s+#(\d+).*Passed\s+(\d+\.\d+)\s+sec")
-    run_times = {}
-    with open(log_file, "r") as f:
-        for line in f:
-            match = regex.search(line)
-            if match:
-                test_number = int(match.group(1))
-                run_time = float(match.group(2))
-                run_times[test_number] = run_time
+def detect_processors(properties):
+    for prop in properties:
+        if prop["name"] == "PROCESSORS":
+            return int(prop["value"])
 
-    return run_times
+    return 1
+
+
+def detect_timeout(properties):
+    for prop in properties:
+        if prop["name"] == "TIMEOUT":
+            return float(prop["value"])
+
+    return 1
 
 
 def main():
@@ -41,46 +35,70 @@ def main():
         type=int,
         help="Number of chunks to divide the test suite into",
     )
-    parser.add_argument("log_files", type=str, nargs="+", help="Log file(s) to analyze")
+    parser.add_argument(
+        "test_info_file",
+        type=str,
+        help="Test summary file from ctest via ctest --show-only=json-v1",
+    )
+    parser.add_argument(
+        "junit_report",
+        type=str,
+        help="Junit report of the test suite for actual runtime of tests",
+    )
 
     args = parser.parse_args()
-    chunks = args.chunks
-    log_files = args.log_files
 
-    all_run_times = {}
-    for log_file in log_files:
-        all_run_times.update(find_run_time(log_file))
+    # read junit report
+    test_runtime = {}
 
-    # sort the dictionary by keys
-    all_run_times = dict(sorted(all_run_times.items()))
-    if list(all_run_times.keys()) != list(range(1, len(all_run_times) + 1)):
-        raise ValueError("The test numbers in the log files are not contiguous.")
+    if os.path.isfile(args.junit_report):
+        xml = junitparser.JUnitXml.fromfile(args.junit_report)
+        for suite in xml:
+            for case in suite:
+                test_runtime[case.name] = case.time
 
-    average_run_time = sum(all_run_times.values()) / chunks
+    # read general test info
+    with open(args.test_info_file, "r", encoding="utf8") as file:
+        test_summary = json.load(file)
 
+    # determine expected processor runtimes per test
+    expected_test_processor_times = []
+    for test_number, test in enumerate(test_summary["tests"]):
+        properties = test["properties"]
+        name = test["name"]
+        num_procs = detect_processors(properties)
+        timeout = detect_timeout(properties)
+
+        if name in test_runtime:
+            expected_proc_runtime = test_runtime[name] * num_procs
+        else:
+            expected_proc_runtime = timeout * num_procs
+
+        expected_test_processor_times.append(expected_proc_runtime)
+
+    # compute average processor time per chunk
+    average_processor_time = sum(expected_test_processor_times) / args.chunks
+
+    # estimate optimal chunks
     chunks = []
     current_chunk_run_time = 0
     chunk_start = 1
-    for test_number, run_time in all_run_times.items():
-        current_chunk_run_time += run_time
-        if current_chunk_run_time >= average_run_time:
+    for test_number, expected_proc_time in enumerate(
+        expected_test_processor_times, start=1
+    ):
+        current_chunk_run_time += expected_proc_time
+        if current_chunk_run_time >= average_processor_time:
             chunks.append((chunk_start, test_number, current_chunk_run_time))
             chunk_start = test_number + 1
             current_chunk_run_time = 0
 
-    chunks.append((chunk_start, len(all_run_times), current_chunk_run_time))
+    chunks.append(
+        (chunk_start, len(expected_test_processor_times), current_chunk_run_time)
+    )
 
-    for i, chunk in enumerate(chunks):
-        print(
-            f"Chunk {i + 1}: {chunk[0]}-{chunk[1]} (expected run time: {chunk[2]:.2f} sec)"
-        )
-
-    print()
-
-    print("Settings for github workflow:")
-    print("test-chunk: [", end=" ")
+    print("[", end="")
     for chunk in chunks[:-1]:
-        print(f'"{chunk[0]},{chunk[1]}"', end=", ")
+        print(f'"{chunk[0]},{chunk[1]}"', end=",")
     print(f'"{chunks[-1][0]},"]')
 
 
